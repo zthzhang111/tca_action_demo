a0d62990c7e798670a2e9c4b3f20057fdfb7c0a5
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-20 17:38:07 +0800

    demo5

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 try{
-    const label = core.getInput('label')
-    core.info(label)
+   
     core.info('启动TCA......');
 } catch (error){
     core.setFailed(error.message);
