1e6775c21b58073f22ab270d9f74ad2944f0815b
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-23 17:19:42 +0800

    e

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const core = require('@actions/core');
 const shelljs = require('shelljs')
 
 try{
+    core.info('获取参数......');
+    const code_path = core.getInput('code_path');
+    const label = core.getInput('label');
+    core.info(code_path);
+    core.info(label)
     core.info('启动TCA......');
-    shelljs.exec('./tca-client/codepuppy scantask')
+    shelljs.exec('./tca-client/codepuppy scantask');
 } catch (error){
     core.setFailed(error.message);
 }
