ee193d6845bb4abdd3394ad5e295becb6fc81a58
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-23 17:04:25 +0800

    q

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-
 const core = require('@actions/core');
-
+const exec = require('@actions/exec')
 
 try{
-   
     core.info('启动TCA......');
+    exec.exec('./tca-client/codepuppy scantask')
 } catch (error){
     core.setFailed(error.message);
 }
