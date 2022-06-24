11a32e04fe9d67dbe2b154ecd17d2447d31d20d5
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-24 11:34:47 +0800

    7777

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const shelljs = require('shelljs')
 const exec = require('child_process').exec
 
 try{
+    core.info(exec.cwd())
     const cmd = 'codepuppy quickinit'
     const cwd = 'tca-client'
     exec(cmd, { cwd }, function (error, stdout, stderr){})
