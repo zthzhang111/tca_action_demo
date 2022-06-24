e4c3cbca8fc47161336c348fbe1add338fd10fac
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-24 11:00:24 +0800

    222

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ try{
     core.info('clinet初始化中......')
     shelljs.exec('./tca-client/codepuppy quickinit')
     core.info('client初始化完成')
-    
+
     core.info('获取参数......');
     const code_path = core.getInput('code_path');
     const label = core.getInput('label');
     core.info(code_path);
     core.info(label)
     core.info('启动TCA......');
-    shelljs.exec('./tca-client/codepuppy scantask');
+    shelljs.exec('./tca-client/codepuppy quickscan --label' + label + '-s ./');
 } catch (error){
     core.setFailed(error.message);
 }
