000a47731087cb84c1f5e4d166a3798eb3ea922c
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-28 10:55:15 +0800

    123

--- tca-client/tca.js
+++ tca-client/tca.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core')
+const shelljs = require('shelljs')
 
 exports.tca_init = tca_init;
 function tca_init(){
-    core.info('调用tca.js')
+    core.info(__dirname)
+    shelljs.exec('./codepuppy quickinit')
+    core.info('tca初始化结束')
 }
