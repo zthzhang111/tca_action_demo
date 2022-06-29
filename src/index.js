cc685c175ff225627d239b4b98dfe4bf20ff6920
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 15:22:13 +0800

    1

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ try{
     const label = core.getInput('label')
     const cmd = './codepuppy quickinit --label ' + label
     const cwd = process.cwd()+ '/tca-client'
+    try {
+        child_process.exec('npm ls')
+    } catch (error) {
+        core.error(error.message)
+    }
     child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
-            console.log(stderr)
+            core.error(stderr)
             return
         }
-        console.log(stderr)
+        core.info(stderr)
     })
     setTimeout(() => {
-        console.log('初始化完成........')
+        core.info('初始化完成........')
     }, 100);
-
-
-
 } catch (error){
-    core.setFailed(error.message);
+    core.error(error.message);
 }
