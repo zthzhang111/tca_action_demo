c7b073ba88f3615439f27fcaa818145d3bcfe193
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 17:46:01 +0800

    1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ try{
     const cmd = './codepuppy quickinit --label ' + label
     const cwd = __dirname + '/../tca-client'
     shelljs.exec('ls')
+    core.info(process.cwd())
     core.info(cwd)
     core.info(__dirname)
     core.info('工具初始化中........')
