c247a8e00a17d5c730a26437bb9e59a87f8e3f59
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 09:55:14 +0800

    12345

--- tca-client/tca.js
+++ tca-client/tca.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core')
 const shelljs = require('shelljs')
-const child_process = require('child_process')
+const child_process = require('child_process').exec
 
 exports.tca_init = tca_init;
 function tca_init(){
-    core.info(process.cwd() + '/tca-client')
-    child_process.exec('./codepuppy quickinit', process.cwd()+ '/tca-client')
-    shelljs.exec('./tca-client/codepuppy quickinit')
-    shelljs.exec('./codepuppy quickinit')
+    const cmd = './codepuppy quickinit'
+    const cwd = process.cwd()+ '/tca-client'
+    child_process(cmd, { cwd }, function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        core.info('run ${cwd} successfully')
+    })
     core.info('tca初始化结束')
 }
