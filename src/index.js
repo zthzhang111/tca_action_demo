01fd16a909f53aef87f1ffc406c4d4257cbd00e3
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 17:09:41 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 const core = require('@actions/core')
 const shelljs = require('shelljs')
+const child_process = require('child_process')
 
-const request = require('request')
-const fs = require('fs')
-
-function downloadFile(url, fileName, callback){
-    const stream = fs.createWriteStream(fileName)
-    request(url).pipe(stream).on('close', callback)
-}
 try{
-    downloadFile(
-        'https://mirrors.tencent.com/repository/generic/tca/release/opensource/v20220613.1/tca-client-v20220613.1-x86_64-linux.zip'
-        ,'tca-client'
-        ,function(){
-        core.info('下载完毕')
+    child_process.execSync(
+        'wget https://mirrors.tencent.com/repository/generic/tca/release/opensource/v20220613.1/tca-client-v20220613.1-x86_64-linux.zip'
+        ,process.cwd()
+        ,function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        core.info(stderr)
     })
 
     const label = core.getInput('label')
     const cmd = './codepuppy quickinit --label ' + label
     const cwd = process.cwd()+ '/tca-client'
-
     core.info('工具初始化中........')
-    const child_process = require('child_process')
     child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
@@ -32,7 +28,6 @@ try{
     setTimeout(() => {
         core.info('初始化完成........')
     }, 100);
-
 } catch (error){
     core.error(error.message);
 }
