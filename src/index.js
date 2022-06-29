c57d18ca6ff15697148914bbc8c458a262f643c7
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 17:36:46 +0800

    123

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ const core = require('@actions/core')
 const shelljs = require('shelljs')
 const child_process = require('child_process')
 
-try{
-    child_process.execSync(
-        'wget https://mirrors.tencent.com/repository/generic/tca/release/opensource/v20220613.1/tca-client-v20220613.1-x86_64-linux.zip'
-        ,process.cwd()
-        ,function(error, stdout, stderr){
-        if (error){
-            core.error(stderr)
-            return
-        }
-        core.info(stderr)
-    })
 
+try{
     const label = core.getInput('label')
     const cmd = './codepuppy quickinit --label ' + label
-    const cwd = process.cwd()+ '/tca-client'
+    const cwd = __dirname + '/../tca-client'
+    core.info(cwd)
     core.info('工具初始化中........')
     child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
