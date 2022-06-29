15f159e040216da680e3f55f97de3b935f88be95
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 16:50:07 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core')
 const shelljs = require('shelljs')
-const child_process = require('child_process')
 
 const request = require('request')
 const fs = require('fs')
@@ -12,9 +11,9 @@ function downloadFile(url, fileName, callback){
 try{
     downloadFile(
         'https://mirrors.tencent.com/repository/generic/tca/release/opensource/v20220613.1/tca-client-v20220613.1-x86_64-linux.zip'
-        ,'tca-client.zip'
+        ,'tca-client'
         ,function(){
-        core.info(fileName + '下载完毕')
+        core.info('下载完毕')
     })
 
     const label = core.getInput('label')
@@ -22,6 +21,7 @@ try{
     const cwd = process.cwd()+ '/tca-client'
 
     core.info('工具初始化中........')
+    const child_process = require('child_process')
     child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
@@ -32,6 +32,7 @@ try{
     setTimeout(() => {
         core.info('初始化完成........')
     }, 100);
+
 } catch (error){
     core.error(error.message);
 }
