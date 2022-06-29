0d91ff827dcc1ade138e300a7e5a670f80469c1b
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 16:01:29 +0800

    1

--- src/index.js
+++ src/index.js
@@ -3,38 +3,14 @@ const shelljs = require('shelljs')
 const child_process = require('child_process')
 
 try{
-    core.info('工具初始化中........')
     const label = core.getInput('label')
     const cmd = './codepuppy quickinit --label ' + label
     const cwd = process.cwd()+ '/tca-client'
-    try {
-        child_process.exec('npm init -y',{ cwd },function(error,stdout,stderr){
-            if(error){
-                core.error(stderr + '错空1')
-                return
-            }
-            core.info(stderr + '对空1')
-        })
-        child_process.exec('npm install @actions/core', process.cwd() ,function(error,stdout,stderr){
-            if(error){
-                core.error(stderr + '错空2')
-                return
-            }
-            core.info(stderr + '对空2')
-        })
-        child_process.exec('npm install @actions/github', process.cwd(), function(error,stdout,stderr){
-            if(error){
-                core.error(stderr + '错空3')
-                return
-            }
-            core.info(stderr + '对空3')
-        })
-    } catch (error) {
-        core.error(error.message)
-    }
+    core.info('工具初始化中........')
     child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
+            core.info('error_one')
             return
         }
         core.info(stderr)
@@ -43,5 +19,6 @@ try{
         core.info('初始化完成........')
     }, 100);
 } catch (error){
+    core.error('error_two')
     core.error(error.message);
 }
