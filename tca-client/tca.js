63a262677c05b60a5fba18f8e8fdc63167e0ce1f
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 10:04:06 +0800

    xx

--- tca-client/tca.js
+++ tca-client/tca.js
@@ -8,7 +8,6 @@ function tca_init(){
     const cwd = process.cwd()+ '/tca-client'
     child_process(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
-            core.error(stderr)
             return
         }
         core.info('run ${cwd} successfully')
