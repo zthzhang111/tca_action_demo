ed45b890192f01e2e2c0aad1139027ab31f6ffa1
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 18:00:45 +0800

    1

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const child_process = require('child_process')
 
 try{
     const label = core.getInput('label')
-    const cmd = './codepuppy quickinit --label ' + label
+    const cmd_init = './codepuppy quickinit --label ' + label
     const cwd = __dirname + '/../tca-client'
     shelljs.exec('ls')
-    core.info(process.cwd())
-    core.info(cwd)
-    core.info(__dirname)
+    // core.info(process.cwd())  用户库的路径（代码扫描的目录）
+    // core.info(cwd) 拼接出来的客户端目录
+    // core.info(__dirname) 执行文件的所在路径，不带文件名
     core.info('工具初始化中........')
-    child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
+    child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
             return
@@ -23,5 +23,22 @@ try{
         core.info('初始化完成........')
     }, 100);
 } catch (error){
+    core.error(error.message);
+}
+
+try {
+    const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
+    core.info('开始执行代码分析........')
+    child_process.execSync(cmd_scan, { cwd }, function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        core.info(stderr)
+    })
+    setTimeout(() => {
+        core.info('代码分析执行完毕........')
+    }, 100);
+} catch (error) {
     core.error(error.message);
 }
