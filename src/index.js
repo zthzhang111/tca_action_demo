1ca40090ed79d34532042b4eb806b939bb234f96
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-29 10:50:09 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core')
 const shelljs = require('shelljs')
-const child_process = require('child_process').execSync
+const child_process = require('child_process')
 
 try{
     core.info('工具初始化中........')
     const label = core.getInput('label')
     const cmd = './codepuppy quickinit --label ' + label
     const cwd = process.cwd()+ '/tca-client'
-    child_process(cmd, { cwd }, function(error, stdout, stderr){
+    child_process.execSync(cmd, { cwd }, function(error, stdout, stderr){
         if (error){
             console.log(stderr)
             return
