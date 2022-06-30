0555000a29192a4507b5ece9a43a1b799b732482
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-30 09:34:24 +0800

    111

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ try{
     // core.info(process.cwd())  用户库的路径（代码扫描的目录）
     // core.info(cwd) 拼接出来的客户端目录
     // core.info(__dirname) 执行文件的所在路径，不带文件名
-    core.info('工具初始化中........')
     child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
@@ -18,16 +17,12 @@ try{
         }
         core.info(stderr)
     })
-    setTimeout(() => {
-        core.info('初始化完成........')
-    }, 100);
 } catch (error){
     core.error(error.message);
 }
 
 try {
     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
-    core.info('开始执行代码分析........')
     child_process.execSync(cmd_scan, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
@@ -35,9 +30,6 @@ try {
         }
         core.info(stderr)
     })
-    setTimeout(() => {
-        core.info('代码分析执行完毕........')
-    }, 100);
 } catch (error) {
     core.error(error.message);
 }
