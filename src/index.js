e54bf5b6c71161f69ae98c4ab3ecdda18e135aee
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-30 11:10:40 +0800

    2222

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ var request = require('request')
 var shelljs = require('shelljs')
 const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
 const fileName = 'tca.zip'
-const dir = process.cwd()
+const dir = '/Users/zhangtenghui/Desktop/A'
 shelljs.exec('ls')
 
 let stream = fs.createWriteStream(path.join(dir, fileName))
