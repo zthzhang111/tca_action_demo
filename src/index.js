dfd119b7a84716b0c038a29ece2941271412c8f8
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-30 10:51:32 +0800

    2222

--- src/index.js
+++ src/index.js
@@ -4,32 +4,49 @@ const child_process = require('child_process')
 
 const label = core.getInput('label')
 const cwd = __dirname + '/../tca-client'
-try{
-    const cmd_init = './codepuppy quickinit --label ' + label
-    shelljs.exec('ls')
-    // core.info(process.cwd())  用户库的路径（代码扫描的目录）
-    // core.info(cwd) 拼接出来的客户端目录
-    // core.info(__dirname) 执行文件的所在路径，不带文件名
-    child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
-        if (error){
-            core.error(stderr)
-            return
-        }
-        core.info(stderr)
-    })
-} catch (error){
-    core.error(error.message);
-}
 
-try {
-    const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
-    child_process.execSync(cmd_scan, { cwd }, function(error, stdout, stderr){
-        if (error){
-            core.error(stderr)
-            return
-        }
-        core.info(stderr)
-    })
-} catch (error) {
-    core.error(error.message);
-}
+
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+
+const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+const fileName = 'tca.zip'
+const dir = __dirname
+core.info(__dirname)
+
+let stream = fs.createWriteStream(path.join(dir, fileName))
+request(url).pipe(stream).on('close', function(error){
+    console.log(fileName + '下载完毕')
+})
+
+
+
+
+
+
+// try{
+//     const cmd_init = './codepuppy quickinit --label ' + label
+//     child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
+//         if (error){
+//             core.error(stderr)
+//             return
+//         }
+//         core.info(stderr)
+//     })
+// } catch (error){
+//     core.error(error.message);
+// }
+
+// try {
+//     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
+//     child_process.execSync(cmd_scan, { cwd }, function(error, stdout, stderr){
+//         if (error){
+//             core.error(stderr)
+//             return
+//         }
+//         core.info(stderr)
+//     })
+// } catch (error) {
+//     core.error(error.message);
+// }
