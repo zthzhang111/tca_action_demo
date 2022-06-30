3ed6ae144d80ee6277babf4cde80f7365fa50f48
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-06-30 11:40:49 +0800

    222

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 const core = require('@actions/core')
 const child_process = require('child_process')
 
+
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+var shelljs = require('shelljs')
+const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+const fileName = 'tca.zip'
+const dir = process.pwd()+ '/lib'
+
+let stream = fs.createWriteStream(path.join(dir, fileName))
+request(url).pipe(stream).on('close', function(error){
+    console.log(fileName + '下载完毕')
+})
+
+
+
+
 const label = core.getInput('label')
 const cwd = __dirname + '/../tca-client'
-// try{
-//     const cmd_init = './codepuppy quickinit --label ' + label
-//     child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
-//         if (error){
-//             core.error(stderr)
-//             return
-//         }
-//         core.info(stderr)
-//     })
-// } catch (error){
-//     core.error(error.message);
-// }
+try{
+    const cmd_init = './codepuppy quickinit --label ' + label
+    child_process.execSync('ls', { dir }, function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        core.info(stderr)
+    })
+} catch (error){
+    core.error(error.message);
+}
 
 // try {
 //     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
@@ -30,17 +47,3 @@ const cwd = __dirname + '/../tca-client'
 // }
 
 
-
-var fs = require('fs')
-var path = require('path')
-var request = require('request')
-var shelljs = require('shelljs')
-const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
-const fileName = 'tca.zip'
-const dir = '/Users/zhangtenghui/Desktop/A'
-shelljs.exec('ls')
-
-let stream = fs.createWriteStream(path.join(dir, fileName))
-request(url).pipe(stream).on('close', function(error){
-    console.log(fileName + '下载完毕')
-})
