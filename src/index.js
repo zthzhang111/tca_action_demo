1ab6b8c9bf387dda280ec4187c546d91f70ee840
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-01 10:53:00 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const core = require('@actions/core')
 const child_process = require('child_process')
 
 
-var fs = require('fs')
-var path = require('path')
-var request = require('request')
-var shelljs = require('shelljs')
-const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
-const fileName = 'tca.zip'
-const dir = process.cwd()+ '/lib'
-core.info(dir)
-shelljs.exec('ls ' + dir)
-let stream = fs.createWriteStream(path.join(dir, fileName))
-request(url).pipe(stream).on('close', function(error){
-    console.log(fileName + '下载完毕')
-})
+// var fs = require('fs')
+// var path = require('path')
+// var request = require('request')
+// var shelljs = require('shelljs')
+// const url = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+// const fileName = 'tca.zip'
+// const dir = process.cwd()+ '/lib'
+// core.info(dir)
+// shelljs.exec('ls ' + dir)
+// let stream = fs.createWriteStream(path.join(dir, fileName))
+// request(url).pipe(stream).on('close', function(error){
+//     console.log(fileName + '下载完毕')
+// })
 
 
 
